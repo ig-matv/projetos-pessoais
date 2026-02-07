528d2caa2f6ea0fd1e38ba680e6e5df87332f231
Igor <matvicioigor550@gmail.com>
2026-02-07 04:30:18 -0300

    Create script.js

---
+++ Takilinks/script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll(".link-btn");
+
+buttons.forEach(button =>{
+    button.addEventListener(
+        "click", () => {
+            console.log("Você clicou em:", button.textContent);
+        }
+    )
+})
